7def1b96a09f5c28e2c074e0667077c2da32dab3
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-09-06 19:17:32 +0200

    Eliminación del condicional

---
+++ src/1_Proposicional/Eliminacion_del_condicional.lean
@@ -0,0 +1,64 @@
+-- Eliminación del condicional en Lean
+-- =====================================================================
+
+-- ---------------------------------------------------------------------
+-- Ejercicio 1. Demostrar que 
+--    (P → Q), P ⊢ Q. 
+-- ---------------------------------------------------------------------
+
+import tactic            
+variables (P Q : Prop)   
+
+-- 1ª demostración
+example  
+  (h1 : P → Q) 
+  (h2 : P) 
+  : Q :=
+begin
+  apply h1,
+  exact h2,
+end 
+
+-- 2ª demostración
+example  
+  (h1 : P → Q) 
+  (h2 : P) 
+  : Q :=
+begin
+  exact h1 h2,
+end 
+
+-- 3ª demostración
+example  
+  (h1 : P → Q) 
+  (h2 : P) 
+  : Q :=
+by exact h1 h2
+
+-- 4ª demostración
+example  
+  (h1 : P → Q) 
+  (h2 : P) 
+  : Q :=
+h1 h2
+
+-- 5ª demostración
+example  
+  (h1 : P → Q) 
+  (h2 : P) 
+  : Q :=
+by tauto
+
+-- 6ª demostración
+example  
+  (h1 : P → Q) 
+  (h2 : P) 
+  : Q :=
+by finish
+
+-- 7ª demostración
+example  
+  (h1 : P → Q) 
+  (h2 : P) 
+  : Q :=
+by solve_by_elim
