c9df43fed6576a7d86d5d4ed03fb36ad29e0920e
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-09-12 14:20:52 +0200

    Introducción del condicional 2

---
+++ src/1_Proposicional/Reglas_de_la_conjuncion.lean
@@ -0,0 +1,124 @@
+-- Reglas de la conjunción
+-- ================================================
+
+-- ------------------------------------------------
+-- Demostrar que
+--    P ∧ Q, R ⊢ Q ∧ R
+-- ------------------------------------------------
+
+import tactic            
+
+variables (P Q R : Prop)
+
+-- 1ª demostración
+-- ===============
+
+example  
+  (hPQ : P ∧ Q)
+  (hR : R)
+  : Q ∧ R :=
+have hQ : Q, 
+  from and.right hPQ,
+show Q ∧ R,  
+  from and.intro hQ hR
+
+-- 2ª demostración
+-- ===============
+
+example  
+  (hPQ : P ∧ Q)
+  (hR : R)
+  : Q ∧ R :=
+have hQ : Q, 
+  from hPQ.right,
+show Q ∧ R,  
+  from ⟨hQ, hR⟩
+
+-- 3ª demostración
+-- ===============
+
+example  
+  (hPQ : P ∧ Q)
+  (hR : R)
+  : Q ∧ R :=
+have hQ : Q, 
+  from hPQ.2,
+show Q ∧ R,  
+  from ⟨hQ, hR⟩
+
+-- 4ª demostración
+-- ===============
+
+example  
+  (hPQ : P ∧ Q)
+  (hR : R)
+  : Q ∧ R :=
+have hQ : Q :=
+  hPQ.2,
+show Q ∧ R,  
+  from ⟨hQ, hR⟩
+
+-- 5ª demostración
+-- ===============
+
+example  
+  (hPQ : P ∧ Q)
+  (hR : R)
+  : Q ∧ R :=
+show Q ∧ R,  
+  from ⟨hPQ.2, hR⟩
+
+-- 6ª demostración
+-- ===============
+
+example  
+  (hPQ : P ∧ Q)
+  (hR : R)
+  : Q ∧ R :=
+⟨hPQ.2, hR⟩
+
+-- 7ª demostración
+-- ===============
+
+example  
+  (hPQ : P ∧ Q)
+  (hR : R)
+  : Q ∧ R :=
+begin
+  split,
+  { cases hPQ with _ hQ,
+    exact hQ, },
+  { exact hR, },
+end
+
+-- 8ª demostración
+-- ===============
+
+example  
+  (hPQ : P ∧ Q)
+  (hR : R)
+  : Q ∧ R :=
+begin
+  split,
+  { cases hPQ,
+    assumption, },
+  { assumption, },
+end
+
+-- 9ª demostración
+-- ===============
+
+example  
+  (hPQ : P ∧ Q)
+  (hR : R)
+  : Q ∧ R :=
+by tauto
+
+-- 10ª demostración
+-- ===============
+
+example  
+  (hPQ : P ∧ Q)
+  (hR : R)
+  : Q ∧ R :=
+by finish
