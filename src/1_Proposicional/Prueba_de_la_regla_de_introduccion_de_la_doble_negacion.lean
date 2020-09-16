0f9dccf2bfd095dfa0ea38c6d415b37cc52bdc71
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-09-16 12:57:22 +0200

    Pruebas de la regla de introducción de la doble negación: P ⊢ ¬¬P

---
+++ src/1_Proposicional/Prueba_de_la_regla_de_introduccion_de_la_doble_negacion.lean
@@ -0,0 +1,71 @@
+-- Regla de introducción de la doble negación
+-- ==========================================
+
+-- Ej. 1. Demostrar
+--    P ⊢ ¬¬P
+
+import tactic
+variable (P : Prop)
+
+-- 1ª demostración
+example
+  (h1 : P)
+  : ¬¬P :=
+not.intro 
+  ( assume h2 : ¬P,
+    show false,
+      from h2 h1 )
+
+-- 2ª demostración
+example
+  (h1 : P)
+  : ¬¬P :=
+assume h2: ¬P,
+show false,
+  from h2 h1
+
+-- 3ª demostración
+example
+  (h1 : P)
+  : ¬¬P :=
+assume h2: ¬P, h2 h1
+
+-- 4ª demostración
+example
+  (h1 : P)
+  : ¬¬P :=
+λ h2, h2 h1
+
+-- 5ª demostración
+example
+  (h1 : P)
+  : ¬¬P :=
+not_not.mpr h1
+
+-- 6ª demostración
+example
+  (h1 : P)
+  : ¬¬P :=
+not_not_intro h1
+
+-- 7ª demostración
+example
+  (h1 : P)
+  : ¬¬P :=
+begin
+  intro h2,
+  exact h2 h1,
+end
+
+-- 8ª demostración
+example
+  (h1 : P)
+  : ¬¬P :=
+by tauto
+
+-- 9ª demostración
+example
+  (h1 : P)
+  : ¬¬P :=
+by finish
+
