032d6dfdef677d3d8bcbad7940331b6b73253e85
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-09-21 11:49:11 +0200

    Pruebas de P → (Q → P)

--- src/1_Proposicional/Pruebas_de_P→(Q→P).lean
+++ src/1_Proposicional/Pruebas_de_P→(Q→P).lean
@@ -4,3 +4,72 @@
 -- Ej. 1. Demostrar
 --    P → (Q → P)
 
+import tactic
+
+variables (P Q : Prop)
+
+-- 1ª demostración
+example : P → (Q → P) :=
+assume (h1 : P),
+show Q → P, from 
+  ( assume h2 : Q,
+    show P, from h1)
+
+-- 2ª demostración
+example : P → (Q → P) :=
+assume (h1 : P),
+show Q → P, from 
+  ( assume h2 : Q, h1)    
+
+-- 3ª demostración
+example : P → (Q → P) :=
+assume (h1 : P),
+show Q → P, from 
+  ( λ h2, h1)      
+
+-- 4ª demostración
+example : P → (Q → P) :=
+assume (h1 : P), (λ h2, h1)        
+
+-- 5ª demostración
+example : P → (Q → P) :=
+λ h1, λ h2, h1        
+
+-- 6ª demostración
+example : P → (Q → P) :=
+λ h1 h2, h1        
+
+-- 7ª demostración
+example : P → (Q → P) :=
+λ h _, h        
+
+-- 8ª demostración
+example : P → (Q → P) :=
+imp_intro
+
+-- 9ª demostración
+example : P → (Q → P) :=
+begin
+  intro h1,
+  intro h2,
+  exact h1,
+end 
+
+-- 10ª demostración
+example : P → (Q → P) :=
+begin
+  intros h1 h2,
+  exact h1,
+end 
+
+-- 6ª demostración
+example : P → (Q → P) :=
+λ h1 h2, h1
+
+-- 11ª demostración
+example : P → (Q → P) :=
+by tauto
+
+-- 12ª demostración
+example : P → (Q → P) :=
+by finish
