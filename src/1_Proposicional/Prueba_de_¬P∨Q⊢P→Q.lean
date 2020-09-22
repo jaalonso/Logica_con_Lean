fe721c95042c508fbb8a46897b9353b3b37b2e6c
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-09-22 13:02:39 +0200

    Prueba de ¬P ∨ Q ⊢ P → Q

--- src/1_Proposicional/Prueba_de_¬P∨Q⊢P→Q.lean
+++ src/1_Proposicional/Prueba_de_¬P∨Q⊢P→Q.lean
@@ -12,4 +12,128 @@ variables (P Q : Prop)
 example 
   (h1 : ¬P ∨ Q)
   : P → Q :=
-sorry
+assume h2 : P,
+or.elim h1 
+  ( assume h3 : ¬P,
+    have h4 : false,
+      from h3 h2,
+    show Q, 
+      from false.elim h4) 
+  ( assume h5 : Q,
+    show Q, from h5)
+
+-- 2ª demostración
+example 
+  (h1 : ¬P ∨ Q)
+  : P → Q :=
+assume h2 : P,
+or.elim h1 
+  ( assume h3 : ¬P,
+    have h4 : false,
+      from h3 h2,
+    show Q, 
+      from false.elim h4) 
+  ( assume h5 : Q, h5)
+
+-- 3ª demostración
+example 
+  (h1 : ¬P ∨ Q)
+  : P → Q :=
+assume h2 : P,
+or.elim h1 
+  ( assume h3 : ¬P,
+    have h4 : false,
+      from h3 h2,
+    show Q, 
+      from false.elim h4) 
+  ( λ h5, h5)
+
+-- 4ª demostración
+example 
+  (h1 : ¬P ∨ Q)
+  : P → Q :=
+assume h2 : P,
+or.elim h1 
+  ( assume h3 : ¬P,
+    have h4 : false,
+      from h3 h2,
+    show Q, 
+      from false.elim h4) 
+  id
+    
+-- 5ª demostración
+example 
+  (h1 : ¬P ∨ Q)
+  : P → Q :=
+assume h2 : P,
+or.elim h1 
+  ( assume h3 : ¬P,
+    show Q, 
+      from false.elim (h3 h2)) 
+  id
+        
+-- 6ª demostración
+example 
+  (h1 : ¬P ∨ Q)
+  : P → Q :=
+assume h2 : P,
+or.elim h1 
+  ( assume h3 : ¬P, false.elim (h3 h2)) 
+  id
+        
+-- 7ª demostración
+example 
+  (h1 : ¬P ∨ Q)
+  : P → Q :=
+assume h2 : P,
+or.elim h1 
+  ( λ h3, false.elim (h3 h2)) 
+  id
+        
+-- 8ª demostración
+example 
+  (h1 : ¬P ∨ Q)
+  : P → Q :=
+λ h2, or.elim h1 (λ h3, false.elim (h3 h2)) id
+
+-- 9ª demostración
+example 
+  (h1 : ¬P ∨ Q)
+  : P → Q :=
+imp_iff_not_or.mpr h1
+
+-- 10ª demostración
+example 
+  (h1 : ¬P ∨ Q)
+  : P → Q :=
+begin
+  intro h2,
+  cases h1 with h3 h4,
+  { apply false.rec,
+    exact h3 h2, },
+  { exact h4, },
+end
+
+-- 11ª demostración
+example 
+  (h1 : ¬P ∨ Q)
+  : P → Q :=
+begin
+  intro h2,
+  cases h1 with h3 h4,
+  { exact false.elim (h3 h2), },
+  { exact h4, },
+end
+
+-- 12ª demostración
+example 
+  (h1 : ¬P ∨ Q)
+  : P → Q :=
+by tauto
+
+-- 13ª demostración
+example 
+  (h1 : ¬P ∨ Q)
+  : P → Q :=
+by finish
+
