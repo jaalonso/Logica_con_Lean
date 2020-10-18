fb02f4f2e95995623e45528f2042f92342148017
Jose A. Alonso <JoseA.Alonso@gmail.com>
2020-10-18 16:53:35 +0200

    Pruebas de la transitividad de la igualdad

---
+++ src/2_LPO/Pruebas_de_la_transitividad_de_la_igualdad.lean
@@ -0,0 +1,109 @@
+-- Pruebas de la transitividad de la igualdad
+-- ==========================================
+
+import tactic
+
+variable  (U : Type)
+variables (x y z : U)
+
+-- 1ª demostración
+example 
+  (h1 : x = y)
+  (h2 : y = z)
+  : x = z :=
+eq.subst h2 h1
+
+-- 2ª demostración
+example 
+  (h1 : x = y)
+  (h2 : y = z)
+  : x = z :=
+h2 ▸ h1
+
+-- 3ª demostración
+example 
+  (h1 : x = y)
+  (h2 : y = z)
+  : x = z :=
+eq.substr h1 h2
+
+-- 4ª demostración
+example 
+  (h1 : x = y)
+  (h2 : y = z)
+  : x = z :=
+eq.trans h1 h2
+
+-- 5ª demostración
+example 
+  (h1 : x = y)
+  (h2 : y = z)
+  : x = z :=
+begin
+  rw h1,
+  exact h2
+end
+
+-- 6ª demostración
+example 
+  (h1 : x = y)
+  (h2 : y = z)
+  : x = z :=
+begin
+  rw h1,
+  assumption,
+end
+
+-- 7ª demostración
+example 
+  (h1 : x = y)
+  (h2 : y = z)
+  : x = z :=
+begin
+  rwa h1,
+end
+
+-- 8ª demostración
+example 
+  (h1 : x = y)
+  (h2 : y = z)
+  : x = z :=
+by rwa h1
+
+-- 9ª demostración
+example 
+  (h1 : x = y)
+  (h2 : y = z)
+  : x = z :=
+begin
+  rw ←h2,
+  exact h1
+end
+
+-- 10ª demostración
+example 
+  (h1 : x = y)
+  (h2 : y = z)
+  : x = z :=
+begin
+  rw h2 at h1,
+  exact h1
+end
+
+-- 11ª demostración
+example 
+  (h1 : x = y)
+  (h2 : y = z)
+  : x = z :=
+by simp *
+
+-- 12ª demostración
+example 
+  (h1 : x = y)
+  (h2 : y = z)
+  : x = z :=
+-- by hint
+by finish
+
+
+
